ec05956768382265ddb53441cdeaea1d8b68a4f2
DonMehdi <mehdi.koraichi@gmail.com>
2015-10-06 22:53:22 +0200

    changemenrt du fichier js-basics

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -6,16 +6,39 @@
     JS_BASICS.isNumberEven = function(i) {
         // i will be an integer.
         // Return true if it's even, and false if it isn't.
+          return i % 2 === 0;
     };
 
     JS_BASICS.getFileExtension = function(str) {
         // str will be a string, but it may not have a file extension.
         // Return the file extension (with no period) if it has one, otherwise false
+
+        if(str.search(".") != -1)
+        {
+          return str.substring(str.lastIndexOf(".")+1,str.length);
+        }
+        else {
+          return false;
+        }
     };
 
     JS_BASICS.longestString = function(arr) {
         // arr will be an array.
         // Return the longest string in the array
+        var big=0;
+        for (var i = 0; i < arr.length-1; i++)
+        {
+          if (arr[i].length > arr[i+1].length && arr[i].length>=arr[big].length)
+          {
+            big=i;
+          }
+        }
+        if(arr[big].length < arr[arr.length-1])
+          {
+            big=arr.length-1;
+          }
+
+        return arr[big];
     };
 
     JS_BASICS.reverseString = function(str) {
